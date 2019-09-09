6ae637c15f267ed37331b42686a333a2c387f578
徐 <nndhysc@163.com>
2019-09-09 17:16:19 +0800

    lianjie

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.footer_icon').mouseover(function(){
         $(".coder_show").fadeIn("slow");
     })
-    $('.close').click(function(){
+    $('.coder_show').mouseleave(function(){
         $(".coder_show").fadeOut("slow");
 
     })
